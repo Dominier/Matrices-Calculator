35fb2d77ca4a2575c0442555cdb48b82de4df456
Timothy Le <118577440+Dominier@users.noreply.github.com>
2023-06-12 20:51:43 -0700

    Add files via upload

---
+++ src/matrix_main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// WIP Message
+	cout << "This calculator is in progress" << endl;
+	
+	// Enter matrix
+	cout << "Enter 4 numbers that will be formatted as: " << endl;
+	cout << "[ a b ] " << endl;
+	cout << "[ c d ] " << endl;
+	
+
+}
